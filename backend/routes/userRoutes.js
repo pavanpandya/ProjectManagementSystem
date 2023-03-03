37ff536eab7c3414468ae8aaf730acdf0729c37e
nisarg001 <nisarg0606@gmail.com>
2023-03-03 17:24:39 +0530

    delete route updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -392,14 +392,14 @@ router.put("/faculty/:id", auth, async (req, res) => {
   try {
     // Find user by ID
     const user = await User.findById(req.params.id);
-    
+    const role = await User.findById(req.user.userId);
     // If user doesn't exist, return an error
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
     // check if user is admin
-    if (req.user.role !== "admin") {
+    if (role.role !== "admin") {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
@@ -424,13 +424,15 @@ router.delete("/student/:id", auth, async (req, res) => {
     // Find user by ID
     const user = await User.findById(req.params.id);
 
+    const LoggedUser = await User.findById(req.user.userId);
+
     // If user doesn't exist, return an error
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
     // check if user is admin or faculty
-    if (req.user.role !== "admin" && req.user.role !== "faculty") {
+    if (LoggedUser.role !== "admin" && LoggedUser.role !== "faculty") {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
